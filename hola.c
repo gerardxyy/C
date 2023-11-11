814e249bb97c6758018ac823ed196279cee3cfa1
gerardxyy <geraatt7@gmail.com>
2023-11-11 16:27:32 -0600

    V1

---
+++ hola.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+    int main(){
+
+    int num;
+
+    printf("Ingresa un numero\n");
+    scanf("%d",&num);
+
+    if (num%2==0)
+    {
+        printf("Es par");
+
+    }else{
+
+        printf("Es impar");
+    }
+
+}
+    
+
