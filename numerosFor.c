ef00dd1478ac47e52a5f8e561dac2686089cf9f3
gerardxyy <geraatt7@gmail.com>
2023-11-12 17:54:25 -0600

    Ciclo for mejorado

--- numerosFor.c
+++ numerosFor.c
@@ -7,11 +7,16 @@ int i,j;
 main(){
     printf("\t\tIngresa el numero hasta el que quieres contar\n");
     scanf("%d",&j);
-    
+
+
+   if (j>0){
     for (i=1; i<=j; i++){
         printf("%d\n",i);
     }
-
+}
+else{
+    printf("\n\"Numero ivalido\"\n");
+}
 }
 
 
