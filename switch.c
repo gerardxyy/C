c606573923ad8205201e182a27fa1d68a3557d6f
gerardxyy <geraatt7@gmail.com>
2023-11-11 18:33:19 -0600

    Switch

---
+++ switch.c
@@ -0,0 +1,48 @@
+#include "stdio.h"
+
+int opcion;
+
+main(){
+    printf("Ingresa el dia de la semana en numero\n");
+    scanf("%d",&opcion);
+
+    switch (opcion)
+    {
+    case 1:
+        printf("Lunes");
+        break;
+    
+    case 2:
+        printf("Martes");
+        break;
+
+    case 3:
+        printf("Miercoles");
+        break;
+
+    case 4:
+        printf("Jueves");
+        break;
+
+    case 5:
+        printf("Viernes");
+        break;
+
+    case 6:
+        printf("Viernes");
+        break;
+
+    case 7:
+        printf("Sabado");
+        break;
+
+    case 8:
+        printf("Domingo");
+        break;
+
+
+        default:
+        printf("Numero invalido, solo numeros del 1 al 8.");
+        break;
+    }
+}
