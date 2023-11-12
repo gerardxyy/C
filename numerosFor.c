42578a1d165a2ba5283d407c7d14c9f73ed3170a
gerardxyy <geraatt7@gmail.com>
2023-11-12 17:48:43 -0600

    Ciclo for

---
+++ numerosFor.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int i,j;
+
+
+
+main(){
+    printf("\t\tIngresa el numero hasta el que quieres contar\n");
+    scanf("%d",&j);
+    
+    for (i=1; i<=j; i++){
+        printf("%d\n",i);
+    }
+
+}
+
+
+
