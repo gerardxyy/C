9a5c65402b73054a5cdc5095c6cd13a48e90b114
gerardxyy <geraatt7@gmail.com>
2023-11-18 17:34:29 -0600

    Prueba de la funcion gets con strings

---
+++ PruebaStrings.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+        char x[50];
+        
+        system("clear");
+        puts("Ingresa tu nombre\n");
+        gets(x);
+        system("clear");
+        printf("\nTu nombre es: %s\n",x);
+
+        return 0;
+}
--- Upemor/clase_viernes.c
+++ Upemor/clase_viernes.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
 
-int main(){
-        
-        printf("Hola pendejos\n");
 
-return 0;
+int main(){
+        int i;
+        int j;
+
+        puts("Desde que numero quieres contar?\n");
+        scanf("%d",&i);
+        puts("Hasta cual numero?\n");
+        scanf("%d\n",&j);
+        
+        if (i<j){
+                 j=j+1;
+        while (i<j){
+                printf("%d",i);
+                i++;
         }
+        }
+
+        else if (i>j){
+                puts("No se puede pendejo");
+        }
+return 0;
+
+
+        }
+
 
 
