4e4c0169b06a60ffb67f93312e28cd32a4940e75
gerardxyy <geraatt7@gmail.com>
2023-11-18 19:17:38 -0600

    Problema 3 EP3 Resuelto.

---
+++ Upemor/EP3/ITI_IP_1C_OGGO230152_E3.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+int n1,n2,n3,n4;
+int mayor,menor;
+
+        system("clear");
+puts("Ingresa los 4 numeros");
+scanf("%d %d %d %d",&n1,&n2,&n3,&n4);
+
+if(n1>n2 && n1>n3 && n1>n4){
+        mayor=n1;
+        }
+
+else if(n2>n1 && n2>n3 && n1>n4){
+        mayor=n2;
+        }
+
+else if(n3>n1 && n3>n2 && n3>n4){
+        mayor=n3;
+        }
+        
+else if(n4>n1 && n4>n2 && n4>n3){
+        mayor=n4;
+        }
+
+if (n1<n2 && n1<n3 && n1<n4){
+        menor=n1;
+        }
+
+else if (n2<n1 && n2<n3 && n2<n4){
+        menor=n2;
+        }
+
+else if (n3<n1 && n3<n2 && n1<n4){
+        menor=n3;
+        }
+
+else if (n4<n1 && n4<n2 && n4<n2){
+        menor=n4;
+        }
+
+        system("clear");
+        int res;
+        res=mayor+menor;
+        printf("El resultado de %d + %d es: %d\n",mayor,menor,res);
+
+return 0;
+        }
