7189bdffb98b061256b8dcd0f9d2de0d25b83311
gerardxyy <geraatt7@gmail.com>
2023-11-21 22:01:35 -0600

    Programa que convierte un dato entero a uno de tipo hexadecimal

---
+++ ConvertidorIntAHexadecimal.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+int num;
+        puts("Ingresa tu numero para convertirlo a Hexadecimal: \n");
+        scanf("%d",&num);
+
+        printf("Tu numero en hexadecimal es: %x\n",num);
+
+        return 0;
+}
