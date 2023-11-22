275126e78edc56c602d4996174b471d59b0bbd7e
gerardxyy <geraatt7@gmail.com>
2023-11-21 21:43:46 -0600

    Prueba de declaracion de variables globales y locales

--- PruebaVariables.c
+++ PruebaVariables.c
@@ -4,7 +4,7 @@ int i; //Esta es una variable global
 
 int main(){
         int n1=10,n2=5;
-        int suma = n1+n2;
+        int suma = n1+n2; //Estas son las variables LOCALES
 
         printf("%i\n",suma);
 
