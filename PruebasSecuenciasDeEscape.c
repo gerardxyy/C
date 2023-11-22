f9125d51a8f8fc6ddbb87cadd4d3bf9f1a5a4b86
gerardxyy <geraatt7@gmail.com>
2023-11-21 23:08:16 -0600

    Intento de probar todas las sentencias de escape para textos

---
+++ PruebasSecuenciasDeEscape.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (void){
+
+        printf("\rMe llamo gera\n"); // \r es retorno de carro y avance de linea (la verdad no entendi XD)
+        printf("\bMe llamo gera\n"); // \b es retroceso de espacio 
+        printf("\rMe llamo gera\n"); 
+return 0;
+}
