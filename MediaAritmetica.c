0a696347a80f253e92ebabc02b15ac6c53d6e2c6
gerardxyy <geraatt7@gmail.com>
2023-11-24 00:13:08 -0600

    Programa basico para calcular la media aritmetica de 7 numeros dados por el usuario

---
+++ MediaAritmetica.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (void){
+        int n1,n2,n3,n4,n5,n6,n7,media;
+        puts("Ingresa los datos para obtener la media\n"); scanf("%d %d %d %d %d %d %d",&n1,&n2,&n3,&n4,&n5,&n6,&n7);
+        media=(n1+n2+n3+n4+n5+n6+n7)/7;
+        printf("Resultado: %d\n",media);
+
+        }
