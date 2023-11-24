c6096a97e646f662e7bd2af0d22cb0901679af3e
gerardxyy <geraatt7@gmail.com>
2023-11-24 00:15:09 -0600

    Conversor de decimal a octal.

---
+++ DecimalToOctal.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+        int num;
+
+        puts("Ingresa tu numero decimal.");
+        scanf("%d",&num);
+
+        printf("El numero octal es %o \n",num);
+
+return 0;
+        }
