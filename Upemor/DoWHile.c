81bb13eb9b7e8ccd791c824c056ea0cb643ae18f
gerardxyy <geraatt7@gmail.com>
2023-11-29 13:27:37 -0600

    Bucle infinito de numeros negativos con el ciclo while

---
+++ Upemor/DoWHile.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void){
+        system("clear");
+        int n=0;
+
+while(n<100){
+
+        printf("%d\n",n);
+        n--;
+}
+
+return 0;
+}
