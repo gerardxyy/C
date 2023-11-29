193101d6d9d90de40486daa4ce9adb192b5d8027
gerardxyy <geraatt7@gmail.com>
2023-11-29 13:39:14 -0600

    Prueba de decrementos e incrementos

---
+++ Upemor/IncrementoYDecremento.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (void){
+        int x,y;
+        x=10;
+        y=10;
+
+        while(x=y){
+        x--;
+        --y;
+        printf("X: %d\tY: %d\n",x,y);
+        }
+
+/* En cocnlusion no cambia nada con el preincremento
+ * y pre decremento XD*/
+
+return 0;
+}
